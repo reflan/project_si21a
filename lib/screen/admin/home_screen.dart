23961b53b4c9e89c66c1638037a42389778b0241
reflan nuari <reflannuari@gmail.com>
2024-05-02 13:14:58 +0700

    commit materi

--- lib/screen/admin/home_screen.dart
+++ lib/screen/admin/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:project_si21a/config/asset.dart';
 
@@ -25,6 +27,239 @@ class _HomeScreenState extends State<HomeScreen> {
               image: AssetImage('asset/images/Web-header-UTI-23.jpg'),
             ),
           ),
+          Container(
+            padding: EdgeInsets.only(top: 25, bottom: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton('User', Icons.people, 0),
+                    IconButton('Menu 2', Icons.mail, 0),
+                    IconButton('Menu 3', Icons.folder, 0),
+                    IconButton('Menu 4', Icons.person_add_alt_1, 0),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton('Menu 5', Icons.account_balance_wallet, 0),
+                    IconButton('Menu 6', Icons.edit_road_outlined, 0),
+                    IconButton('Menu 7', Icons.people, 0),
+                    IconButton('Menu 8', Icons.people, 0),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 10,
+            ),
+            child: Text(
+              "Berita Terkini",
+              style: TextStyle(
+                color: Asset.colorPrimaryDark,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          SizedBox(
+            height: 170,
+            child: ListView(
+              children: [
+                CarouselSlider(
+                  items: [
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/Sekar-Kinasih-040423-1.jpg'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/01/WhatsApp-Image-2023-01-04-at-18.00.09.jpeg'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/Tim-Tari-Teknokrat-120323.jpeg'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/ddaa.png'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/Sekar-Kinasih-040423-1.jpg'),
+                  ],
+                  options: CarouselOptions(
+                    height: 170.0,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enableInfiniteScroll: true,
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    viewportFraction: 0.8,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BeritaTerkini extends StatelessWidget {
+  final String img;
+
+  BeritaTerkini(this.img);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                image: DecorationImage(
+                  image: NetworkImage(img),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                gradient: LinearGradient(
+                  colors: [
+                    Asset.colorPrimaryDark,
+                    Colors.transparent,
+                  ],
+                  end: Alignment.topCenter,
+                  begin: Alignment.bottomCenter,
+                  stops: [0.1, 0.9],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Isi Berita yang ada di setiap konten.',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    '31 Maret 2023',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      // alignment: Alignment(1, 1),
+    );
+  }
+}
+
+class SlideItem extends StatelessWidget {
+  final String img;
+
+  SlideItem(this.img);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey.shade300)),
+      child: Image(
+          image: NetworkImage(
+            img,
+          ),
+          fit: BoxFit.cover),
+    );
+  }
+}
+
+class IconButton extends StatelessWidget {
+  final String nameLabel;
+  final IconData iconLabel;
+  final int index;
+
+  IconButton(this.nameLabel, this.iconLabel, this.index);
+
+  List<Map> _fragment = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            // margin: EdgeInsets.only(bottom: 5),
+            child: Material(
+              borderRadius: BorderRadius.all(
+                Radius.circular(40),
+              ),
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(40),
+                ),
+                onTap: () {},
+                child: Container(
+                  // margin: EdgeInsets.all(5),
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Asset.colorPrimaryDark,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        Icon(
+                          iconLabel,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            child: Text(
+              nameLabel,
+              style: TextStyle(fontSize: 14),
+            ),
+          )
         ],
       ),
     );
